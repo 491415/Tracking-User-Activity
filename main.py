0ecd1f819614efa5442fdf49a6fff978ef3599fd
Manan-200 <manandeopurkar@gmail.com>
2023-02-21 23:03:12 +0530

    Added terminal output

--- main.py
+++ main.py
@@ -18,16 +18,27 @@ w = win32gui
 date = datetime.datetime.now()
 oldWindow = ""
 oldTime = datetime.datetime.now()
-DATA_FILE = "data.json"
 
+DATA_FILE = "data.json"
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
+print(CLEAR)
 while True:
     window = w.GetWindowText(w.GetForegroundWindow()).split("- ")[-1]
     data = loadData(DATA_FILE)
-    
+
     try:
         data[window] += 1
     except:
         data[window] = 0
 
     saveData(DATA_FILE, data)
+
+    print(f"{CLEAR_AND_RETURN}")
+    for key in data:
+        if key:
+            min = data[key]//60
+            sec = data[key] % 60
+            print(f"{key}: {min:02d}:{sec:02d}")
     time.sleep(1)
